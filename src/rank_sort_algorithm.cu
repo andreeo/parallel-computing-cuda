be7da94774bc6a75f224994d02d053cb6862a292
Andreeo <andreeodg7@outlook.es>
2023-08-30 20:33:05 +0200

    fix indent

--- src/rank_sort_algorithm.cu
+++ src/rank_sort_algorithm.cu
@@ -83,7 +83,8 @@ main (int argc, char **argv)
   cudaEventRecord (start, 0);
 
   // call the kernel
-  rank_sort_algorithm <<< block, threadsPerBlock >>> (dev_vecB, dev_vecR, size);
+  rank_sort_algorithm <<< block, threadsPerBlock >>> (dev_vecB, dev_vecR,
+                                                      size);
 
   // wait for compute device to finish
   cudaDeviceSynchronize ();
